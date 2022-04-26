8cc6782b2aa3544b5c24f915cd873eb64a8efdc1
brodriguesbuss <brodriguesbuss@gmail.com>
2022-04-26 15:55:30 -0300

    Atualizado método main para método de teste, também foi adicionado a dependencia JUnit e inserido a validação de resultado ao final do método.

--- src/main/java/br.com.calculadora/CalculadoraTeste.java
+++ src/main/java/br.com.calculadora/CalculadoraTeste.java
@@ -1,14 +1,28 @@
 package br.com.calculadora;
 
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
+/**
+ * Automatização utilizando emulador Android
+ *
+ * O objetivo desta classe é realizar a automatização de uma soma na calculadora
+ * Utilizando o JUnit para realizar a comparação do resultado
+ */
+
 public class CalculadoraTeste {
 
-    public static void main(String[] args) throws MalformedURLException {
+    @Test
+    public  void deveSomarDoisValores() throws MalformedURLException {
+
+        //Parametros para comunicação com emulador e o aplicativo que irá automatizar no Android
+        //Aqui informamos qual o SO do emulador, nome, framework de testes do UI, pasta do aplicativo e aplicativo
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("platformName", "Android");
         desiredCapabilities.setCapability("deviceName", "emulator-5554");
@@ -16,7 +30,31 @@ public class CalculadoraTeste {
         desiredCapabilities.setCapability("appPackage", "com.android.calculator2");
         desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
 
+        // OBS: O uiautomator2 oferece um conjunto de APIs para criar testes de IU que realizam interações em apps de usuário e do sistema
+
+        //Parametros para comunicação do servidor do emulador Android
         AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
 
+        //busca elemento, numero 8
+        MobileElement el1 = (MobileElement) driver.findElementById("com.android.calculator2:id/digit_8");
+        //clica no elemento
+        el1.click();
+        //busca elemento, soma
+        MobileElement el2 = (MobileElement) driver.findElementByAccessibilityId("plus");
+        //clica no elemento
+        el2.click();
+        //busca elemento, numero 9
+        MobileElement el3 = (MobileElement) driver.findElementById("com.android.calculator2:id/digit_9");
+        //clica no elemento
+        el3.click();
+        //busca elemento, resultado
+        MobileElement el4 = (MobileElement) driver.findElementById("com.android.calculator2:id/result");
+
+        //Comparando resultado com JUnit
+        Assert.assertEquals("17", el4.getText());
+
+        //Encerrar sessão com servidor
+        //Esse comando retorna o celular para tela inicial
+        driver.quit();
     }
 }
