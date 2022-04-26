a0c7a04c5763f491e37d5a2af16459a369a13eee
brodriguesbuss <brodriguesbuss@gmail.com>
2022-04-26 15:07:47 -0300

    first commit

---
+++ src/main/java/br.com.calculadora/CalculadoraTeste.java
@@ -0,0 +1,22 @@
+package br.com.calculadora;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class CalculadoraTeste {
+
+    public static void main(String[] args) throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("deviceName", "emulator-5554");
+        desiredCapabilities.setCapability("automationName", "uiautomator2");
+        desiredCapabilities.setCapability("appPackage", "com.android.calculator2");
+        desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+
+    }
+}
